fb1e4e06ea09f8f3dd964c9956e70463177ed8c6
mmazz <mazzantimatiass@gmail.com>
2020-07-20 12:51:15 -0300

    Paper TTT: Agregando ejemplos

--- code/Example3.py
+++ code/Example3.py
@@ -1,18 +1,15 @@
-import sys
-sys.path.append('PathTTT/')
-import time
-from line_profiler import LineProfiler
-import cProfile, pstats, io
 import TTT as thM
 from importlib import reload
+import sys
 
+sys.path.append('PathTTT/')
 reload(thM)
 envM = thM.TrueSkill(draw_probability=0)
-composition = [[["carlos"],["Jorge"]], [["Jorge"],["Luis"]], [["Luis"],["carlos"]]]
-results = [[0,1], [0,1], [0,1]]
-batch = ['2015-03','2015-03','2015-03']
+composition = [[["Stephen"], ["Gustavus"]], [["Gustavus"], ["Matt"]], [["Matt"], ["Stephen"]]]
+results = [[0, 1], [0, 1], [0, 1]]
+batch = ['2015-03', '2015-03', '2015-03']
 
-historyM = envM.history(composition, results, batch,batch_type='month',iterations =10, epsilon=1e-19)
+historyM = envM.history(composition, results, batch, batch_type='month')
 # Information of how the data upload
 historyM.status()
 # Define all variables and make the first TrueSkill pass
