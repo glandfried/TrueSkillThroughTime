897172532d40be2440001a75cdac283d4520cd79
mmazz <mazzantimatiass@gmail.com>
2020-07-20 12:52:26 -0300

    Paper TTT: leves cambios

--- code/Example3.py
+++ code/Example3.py
@@ -1,20 +1,18 @@
-import TTT as thM
-from importlib import reload
+import TTT as th
 import sys
-
 sys.path.append('PathTTT/')
-reload(thM)
-envM = thM.TrueSkill(draw_probability=0)
+
+env = th.TrueSkill(draw_probability=0)
 composition = [[["Stephen"], ["Gustavus"]], [["Gustavus"], ["Matt"]], [["Matt"], ["Stephen"]]]
 results = [[0, 1], [0, 1], [0, 1]]
 batch = ['2015-03', '2015-03', '2015-03']
 
-historyM = envM.history(composition, results, batch, batch_type='month')
+history = env.history(composition, results, batch, batch_type='month')
 # Information of how the data upload
-historyM.status()
+history.status()
 # Define all variables and make the first TrueSkill pass
-historyM.through_time(online=False)
+history.through_time(online=False)
 # Makes convergence in each batch and pass Information back and foward till convergence
-historyM.convergence()
+history.convergence()
 # Print last posterior of each player
-print(historyM.posteriors_player())
+print(history.posteriors_player())
