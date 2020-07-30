c5a63584c363b94a1196ffddf087786c53a3bbfa
mmazz <mazzantimatiass@gmail.com>
2020-07-30 16:11:29 -0300

    Paper TTT: cambios en los ejemplos

--- code/Example2.py
+++ code/Example2.py
@@ -20,8 +20,8 @@ for i in df.index:
     p2_key = str(df.loc[i].Player2)
     prior_1 = player.get(p1_key, globalPrior)
     prior_2 = player.get(p2_key, globalPrior)
-    t1 = [prior_1.foret(1)]  # add noise to prior
-    t2 = [prior_2.foret(1)]
+    t1 = [prior_1.forget(1)]  # add noise to prior
+    t2 = [prior_2.forget(1)]
     result = df.loc[i].Result
     game = env.Game([t1, t2], result)
     t1_post, t2_post = game.posterior
