2e3496102484239f25c309ed4ab3698d632b5399
Gustavo Landfried <gustavolandfried@gmail.com>
2020-10-28 16:51:17 -0300

    TTT: posterior exactas, draw

--- figures/draw.R
+++ figures/draw.R
@@ -19,18 +19,16 @@ epsilon <- 1
 
 D <- seq(mu-3*sigma,mu+3*sigma,0.1)
 dnormal <- dnorm(D,mu,sigma)
-plot(D,dnormal, type="l",lwd=1, xlab=expression(d[1][2]),axes = F,ann = F)
+plot(D,dnormal, type="l",lwd=1, xlab=expression(delta),axes = F,ann = F)
 
 axis(side=2, labels=NA,cex.axis=0.6,tck=0.015)
 axis(side=1, labels=NA,cex.axis=0.6,tck=0.015)
-axis(lwd=0,side=1, at=0,las=0,cex.axis=1.33,line=-0.6)
-axis(lwd=1,side=1, at=mu,labels=expression( d[1][2]) ,las=0,cex.axis=1.33,line=-0.3)
-axis(lwd=1,side=1, at=-epsilon,labels=expression( -epsilon ) ,las=0,cex.axis=1.33,line=-0.3)
+axis(lwd=0,side=1, at=0,las=0,cex.axis=1.75,line=-0.45)
+axis(lwd=1,side=1, at=mu,labels=expression(delta) ,las=0,cex.axis=2,line=-0.3)
+axis(lwd=0,side=1, at=-epsilon*1.3,labels=expression(-epsilon) ,las=0,cex.axis=2.33,line=-0.45)
 
-
-mtext(text ="Density" ,side =2 ,line=2,cex=1.75)
+mtext(text ="Density" ,side =2 ,line=1,cex=1.75)
 abline(v=0,lty=3)
-
 
 base <- rep(0,length(D))
 xx <- c(D[D>=epsilon],rev(D[D>=epsilon]))
@@ -48,9 +46,9 @@ xx <- c(D[D>=epsilon],rev(D[D>=epsilon]))
 yy <- c(base[D>=epsilon],rev(dnormal[D>=epsilon]) )
 polygon(xx,yy,col=rgb(0,0,0,0.25))
 
-text(-epsilon-1.2,0.025,"Lose",cex=1.5)
-text(0,0.025,"Draw",cex=1.5)
-text(epsilon+1.4,0.025,"Win",cex=1.5)
+text(-epsilon-1.2,0.025,"Lose",cex=1.75)
+text(0,0.025,"Draw",cex=1.75)
+text(epsilon+1.4,0.025,"Win",cex=1.75)
 
 ###### PLOT 2
 
@@ -68,7 +66,7 @@ plot(D,dnormal, type="l",lwd=1, axes = F,ann = F)
 axis(side=2, labels=NA,cex.axis=0.6,tck=0.015)
 axis(side=1, labels=NA,cex.axis=0.6,tck=0.015)
 axis(lwd=0,side=1, at=0,las=0,cex.axis=1.33,line=-0.6)
-mtext(text ="Density" ,side =2 ,line=2,cex=1.5)
+mtext(text ="Density" ,side =2 ,line=1,cex=1.75)
 
 base <- rep(0,length(D))
 xx <- c(D[D>=-epsilon & D<=epsilon],rev(D[D>=-epsilon & D<=epsilon]))
@@ -87,6 +85,7 @@ polygon(xx,yy,col=rgb(0,0,0,0.25))
 #######################################
 # end 
 dev.off()
+system(paste("pdfcrop -m '0 0 0 0'",paste0(nombre,".pdf") ,paste0(nombre,".pdf")))
 setwd(oldwd)
 par(oldpar, new=F)
 #########################################
