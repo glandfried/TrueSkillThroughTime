eff42f3d3544cbadf43e1269a90fa24de0b6365b
Gustavo Landfried <gustavolandfried@gmail.com>
2021-02-23 19:12:04 -0500

    TTT: comentarios sin terminar del ejemplo sintético

---
+++ figures/logistic.R
@@ -0,0 +1,21 @@
+oldpar <- par(no.readonly = TRUE)
+oldwd <- getwd()
+this.dir <- dirname(parent.frame(2)$ofile)
+nombre.R <-  sys.frame(1)$ofile
+require(tools)
+nombre <- print(file_path_sans_ext(nombre.R))
+pdf(paste0(nombre,".pdf"), width = 8, height = 5  )
+setwd(this.dir)
+###############################
+
+
+data = read.csv("data/logistic.csv", stringsAsFactors=FALSE)
+
+
+#######################################
+# end 
+dev.off()
+system(paste("pdfcrop -m '0 0 0 0'",paste0(nombre,".pdf") ,paste0(nombre,".pdf")))
+setwd(oldwd)
+par(oldpar, new=F)
+#########################################
