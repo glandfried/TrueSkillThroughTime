d819c51df39135d48d4e2354b6fcdefdea16ccec
Gustavo Landfried <gustavolandfried@gmail.com>
2021-03-26 15:24:58 -0300

    TTT: gráfico provisorio de optimización

--- figures/optimization.R
+++ figures/optimization.R
@@ -11,17 +11,27 @@ setwd(this.dir)
 basic = read.csv("data/atp_optimization.csv", stringsAsFactors=FALSE)
 
 rows = basic[,1]
-cols = seq(0.3,1.2,0.1)
+cols = seq(1.0,5.0,0.25)
 
 basic = data.matrix(basic[,-1])
-rows 
+
+model_evidence <- function(log_ev){
+    n = dim(log_ev)[1] * dim(log_ev)[2]
+    lp = log(1/n)
+    propotional_log_posterior = log_ev + lp #  log p(D|M) + log P(M)  el posterior proporcional
+    c  = max(log_ev + lp) # Constante
+    propotional_log_posterior = propotional_log_posterior  - c #
+    model_posterior = exp(propotional_log_posterior)/sum(exp(propotional_log_posterior))
+    return(model_posterior )
+}
+
 
 min(basic)
 which.max(basic[,dim(basic)[2]])
 
-persp(basic,theta=20,phi=20)
-image(rows,cols,basic)
-contour(rows,cols,basic, add=T)
+persp(model_evidence(basic) ,theta=230,phi=25, xlab="Gamma", ylab="Sigma", zlab="Model probability")
+image(rows,cols,model_evidence(basic))
+contour(rows,cols,model_evidence(basic), add=T)
 
 #######################################
 # end 
