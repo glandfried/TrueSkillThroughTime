9f4e6bd27595a5fadcff8ac08adc6a98de96d0ef
Gustavo Landfried <gustavolandfried@gmail.com>
2021-03-29 15:59:49 -0300

    TTT: nueva figura optimización, y discusión de la figura ATP

--- figures/optimization.R
+++ figures/optimization.R
@@ -8,11 +8,13 @@ pdf(paste0(nombre,".pdf"), width = 8, height = 5  )
 setwd(this.dir)
 ###############################
 
-basic = read.csv("data/atp_optimization.csv", stringsAsFactors=FALSE)
+basic = read.csv("data/atp_optimization_zoom.csv", stringsAsFactors=FALSE)
+
+colnames(basic)
 
 rows = basic[,1]
 cols = seq(1.0,5.0,0.25)
-
+cols = seq(1.0,2.0,0.05)
 basic = data.matrix(basic[,-1])
 
 model_evidence <- function(log_ev){
@@ -26,10 +28,13 @@ model_evidence <- function(log_ev){
 }
 
 
-min(basic)
-which.max(basic[,dim(basic)[2]])
+pmat <- persp(model_evidence(basic) ,theta=230,phi=20, xlab="Gamma", ylab="Sigma", zlab="Model probability",cex.lab=1.5)
 
-persp(model_evidence(basic) ,theta=230,phi=25, xlab="Gamma", ylab="Sigma", zlab="Model probability")
+# tick.start <- trans3d(rows, 1,0, 0.0, pmat)
+# tick.end   <- trans3d(rows, 1,0-0.2, 0.0, pmat)
+# segments(tick.start$x, tick.start$y, tick.end$x, tick.end$y)
+
+
 image(rows,cols,model_evidence(basic))
 contour(rows,cols,model_evidence(basic), add=T)
 
